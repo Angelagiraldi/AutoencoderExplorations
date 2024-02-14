5e4817b64811c0769975cc410136d8659f26d611
Angelagiraldi <giraldiangela@gmail.com>
2024-02-14 16:02:07 +0100

    rewritten with better code approach 1

--- approach_1.py
+++ approach_1.py
@@ -1,128 +1,102 @@
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1' 
-
-import tensorflow as tf
-from tensorflow import keras
-import matplotlib.pyplot as plt
 import numpy as np
-from tensorflow.keras.datasets import fashion_mnist
+import matplotlib.pyplot as plt
+from tensorflow import keras
 from tensorflow.keras.datasets import mnist
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, UpSampling2D, Input, ZeroPadding2D
-from tensorflow.keras.layers import BatchNormalization, LeakyReLU, Activation, Cropping2D, Dense
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, ZeroPadding2D, Cropping2D
 from tensorflow.keras.models import Model
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, TensorBoard, ReduceLROnPlateau
 
-
-from tensorflow.keras.callbacks import ModelCheckpoint
-from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow.keras.callbacks import TensorBoard
-from tensorflow.keras.callbacks import ReduceLROnPlateau
-
-
-
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 path = 'mnist.npz'
-# Loading our data
-with np.load(path, allow_pickle=True) as f:  
-    x_train, y_train = f['x_train'], f['y_train']
-    x_test, y_test = f['x_test'], f['y_test']
 
 
+class Autoencoder:
+    def __init__(self):
+        self.model = self._build_model()
 
-# Understanding the shape of the dataset
-print(x_train[0].shape)
+    def _build_model(self):
+        input_layer = Input(shape=(28, 28, 1))
+        zero_pad = ZeroPadding2D((2, 2))(input_layer)
+        conv1 = Conv2D(16, (3, 3), activation='relu', padding='same')(zero_pad)
+        pool1 = MaxPooling2D((2, 2), padding='same')(conv1)
+        conv2 = Conv2D(16, (3, 3), activation='relu', padding='same')(pool1)
+        pool2 = MaxPooling2D((2, 2), padding='same')(conv2)
+        conv3 = Conv2D(8, (3, 3), activation='relu', padding='same')(pool2)
+        pool3 = MaxPooling2D((2, 2), padding='same')(conv3)
+        conv4 = Conv2D(8, (3, 3), activation='relu', padding='same')(pool3)
+        encoder = MaxPooling2D((2, 2), padding='same')(conv4)
 
-# Visualizing the data
-plt.imshow(x_train[2], cmap="gray")
-plt.savefig('visualization_traindata.pdf')
+        conv_1 = Conv2D(8, (3, 3), activation='relu', padding='same')(encoder)
+        upsample1 = UpSampling2D((2, 2))(conv_1)
+        conv_2 = Conv2D(8, (3, 3), activation='relu', padding='same')(upsample1)
+        upsample2 = UpSampling2D((2, 2))(conv_2)
+        conv_3 = Conv2D(16, (3, 3), activation='relu', padding='same')(upsample2)
+        upsample3 = UpSampling2D((2, 2))(conv_3)
+        conv_4 = Conv2D(1, (3, 3), activation='relu', padding='same')(upsample3)
+        upsample4 = UpSampling2D((2, 2))(conv_4)
+        decoder = Cropping2D((2, 2))(upsample4)
 
+        autoencoder = Model(input_layer, decoder)
+        autoencoder.compile(optimizer='adam', loss='mse')
+        return autoencoder
 
-# Normalizing the data
-x_train = x_train.astype('float32') / 255.0
-x_test = x_test.astype('float32') / 255.0
+    def summary(self):
+        return self.model.summary()
 
+class DataHandler:
+    @staticmethod
+    def load_and_normalize_data():
+        
+        with np.load(path, allow_pickle=True) as f:  
+            x_train, y_train = f['x_train'], f['y_train']
+            x_test, y_test = f['x_test'], f['y_test']
 
-# Creating the input layer with the full shape of the image
-input_layer = Input(shape=(28, 28, 1))
+        x_train = x_train.astype('float32') / 255.
+        x_test = x_test.astype('float32') / 255.
+        return x_train, x_test
 
-# Note: If utilizing a deep neural network without convolution, ensure that the dimensions are multiplied and converted 
-# accordingly before passing through further layers of the encoder architecture.
+    @staticmethod
+    def visualize_data(data, title='Train Data Visualization', save_as='visualization_traindata.pdf'):
+        plt.imshow(data[2], cmap="gray")
+        plt.title(title)
+        plt.savefig(save_as)
+        plt.show()
 
-zero_pad = ZeroPadding2D((2, 2))(input_layer)
+def train_autoencoder(x_train, x_test):
+    callbacks = [
+        ModelCheckpoint("best_model.h5", monitor="val_loss", save_best_only=True),
+        EarlyStopping(monitor="val_loss", patience=8),
+        TensorBoard(log_dir='./autoencoder_logs/'),
+        ReduceLROnPlateau(monitor="val_loss", factor=0.2, patience=4, min_lr=0.000001)
+    ]
 
-# First layer
-conv1 = Conv2D(16, (3, 3), activation='relu', padding='same')(zero_pad)
-pool1 = MaxPooling2D((2, 2), padding='same')(conv1)
+    autoencoder = Autoencoder()
+    autoencoder.model.fit(x_train, x_train, epochs=100, batch_size=128, shuffle=True, validation_data=(x_test, x_test), callbacks=callbacks)
+    return autoencoder
 
-# Second layer
-conv2 = Conv2D(16, (3, 3), activation='relu', padding='same')(pool1)
-pool2 = MaxPooling2D((2, 2), padding='same')(conv2)
+def visualize_results(x_test, decoded_imgs, n=10, save_as='visualization_results.pdf'):
+    plt.figure(figsize=(20, 4))
+    for i in range(n):
+        # Display original
+        plt.subplot(2, n, i + 1)
+        plt.imshow(x_test[i].reshape(28, 28), cmap='gray')
+        plt.axis('off')
 
-# Third layer
-conv3 = Conv2D(8, (3, 3), activation='relu', padding='same')(pool2)
-pool3 = MaxPooling2D((2, 2), padding='same')(conv3)
+        # Display reconstruction
+        plt.subplot(2, n, i + 1 + n)
+        plt.imshow(decoded_imgs[i].reshape(28, 28), cmap='gray')
+        plt.axis('off')
 
-# Final layer
-conv4 = Conv2D(8, (3, 3), activation='relu', padding='same')(pool3)
+    plt.savefig(save_as)
+    plt.show()
 
-# Encoder architecture
-encoder = MaxPooling2D((2, 2), padding='same')(conv4)
-
-
-# First reconstructing decoder layer
-conv_1 = Conv2D(8, (3, 3), activation='relu', padding='same')(encoder)
-upsample1 = UpSampling2D((2, 2))(conv_1)
-
-# Second reconstructing decoder layer
-conv_2 = Conv2D(8, (3, 3), activation='relu', padding='same')(upsample1)
-upsample2 = UpSampling2D((2, 2))(conv_2)
-
-# Third decoder layer
-conv_3 = Conv2D(16, (3, 3), activation='relu', padding='same')(upsample2)
-upsample3 = UpSampling2D((2, 2))(conv_3)
-
-# First reconstructing decoder layer
-conv_4 = Conv2D(1, (3, 3), activation='relu', padding='same')(upsample3)
-upsample4 = UpSampling2D((2, 2))(conv_4)
-
-# Decoder architecture
-decoder = Cropping2D((2, 2))(upsample4)
-
-# Creating and compiling the model
-
-autoencoder = Model(input_layer, decoder)
-autoencoder.compile(optimizer='adam', loss='mse')
-print(autoencoder.summary())
-
-# Creating Callbacks
-tensorboad_results = TensorBoard(log_dir='autoencoder_logs_fashion/')
-checkpoint = ModelCheckpoint("best_model_fashion.h5", monitor="val_loss", save_best_only=True)
-early_stop = EarlyStopping(monitor="val_loss", patience=8, restore_best_weights=False)
-reduce_lr = ReduceLROnPlateau(monitor="val_loss", factor=0.2, patience=4, min_lr=0.000001)
-
-# Training our autoencoder model
-autoencoder.fit(x_train, x_train,
-                epochs=100,
-                batch_size=128,
-                shuffle=True,
-                validation_data=(x_test, x_test),
-                callbacks=[checkpoint, early_stop, tensorboad_results, reduce_lr])
-
-decoded_imgs = autoencoder.predict(x_test)
-
-n = 10
-plt.figure(figsize=(20, 4))
-for i in range(1, n + 1):
-    # Display original
-    ax = plt.subplot(2, n, i)
-    plt.imshow(x_test[i].reshape(28, 28))
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-
-    # Display reconstruction
-    ax = plt.subplot(2, n, i + n)
-    plt.imshow(decoded_imgs[i].reshape(28, 28))
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-plt.show()
-plt.savefig('visualization_results.pdf')
+# Main execution
+if __name__ == "__main__":
+    x_train, x_test = DataHandler.load_and_normalize_data()
+    DataHandler.visualize_data(x_train)
+    
+    autoencoder = train_autoencoder(x_train, x_test)
+    decoded_imgs = autoencoder.model.predict(x_test)
+    
+    visualize_results(x_test, decoded_imgs)
